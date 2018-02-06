86faf29bb94b65d81b6cd68d30340b61fa9cdd8e
Rez <ajmortell@gmail.com>
2018-02-05 21:20:54 -0600

    Computer Scene / Resume Retool
    So I have made a front end here that allows the player to use the
    computer to access the word processing application to make his resume. I
    have messed Ryan C's Resume Scene only in display not code. I am going
    to do a different thing but still use Ryan's code. All for aesthetic
    purposes. Also modifying the file structure a little bit to organize
    better. Some spelling errors. Just anal crap I care about :D

---
+++ Assets/Scripts/Computer/BrowserPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrowserPanel : MonoBehaviour {
+
+    public GameObject panel = null;
+    private bool canOpen = true;
+
+    void Awake() {
+        panel.SetActive(false);
+    }
+
+    public void OnBtnPress() {
+        if (canOpen == true) {
+            canOpen = false;
+            panel.SetActive(true);
+        } else {
+            canOpen = true;
+            panel.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Computer/StartMenuPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuPanel : MonoBehaviour {
+
+    public GameObject panel = null;
+    private bool canOpen = true;
+
+    void Awake() {
+        panel.SetActive(false);
+    }
+
+    public void OnBtnPress() {
+        if (canOpen == true) {
+            canOpen = false;
+            panel.SetActive(true);
+        } else {
+            canOpen = true;
+            panel.SetActive(false);
+        }
+
+    }
+}
