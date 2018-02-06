7df4c038c534d7aa59134fd0fe4f9252ed6268bb
Rez <ajmortell@gmail.com>
2018-02-06 15:11:44 -0600

    Clock
    Added some code that updates the clock in real time with your PC

--- Assets/Scripts/Computer/Clock.cs
+++ Assets/Scripts/Computer/Clock.cs
@@ -3,30 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+/// <summary>
+/// Gets the current time and updates the in game PC Clock
+/// </summary>
 public class Clock : MonoBehaviour {
 
     string time;
     GameObject clock = null;
 
-    // Use this for initialization
     void Awake () {
         time = DateTime.Now.ToShortTimeString();
         clock = gameObject;
         clock.GetComponent<Text>().text = time;
-
-        //UpdateClock();
 	}
-	
-    public void UpdateClock()
-    {
-        
-        
-        
-    }
 
-	// Update is called once per frame
 	void Update () {
-		
-	}
+       time = DateTime.Now.ToShortTimeString();
+       clock.GetComponent<Text>().text = time;
+    }
 }
