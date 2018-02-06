39633259175ba75dd2b2315e9b7e6de425cc438d
Rez <ajmortell@gmail.com>
2018-02-06 15:01:38 -0600

    PC Clock
    Added a working clock to the PC :)

---
+++ Assets/Scripts/Computer/Clock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour {
+
+    string time;
+    GameObject clock = null;
+
+    // Use this for initialization
+    void Awake () {
+        time = DateTime.Now.ToShortTimeString();
+        clock = gameObject;
+        clock.GetComponent<Text>().text = time;
+
+        //UpdateClock();
+	}
+	
+    public void UpdateClock()
+    {
+        
+        
+        
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
