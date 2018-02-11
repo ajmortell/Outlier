fe128361295640e84a2d3c1c80c9791cebb28ae9
greyHat71 <mrchexmix@gmail.com>
2018-02-11 07:48:08 -0600

    More Resume Stuff
    Tabbing now jumps the scroll when tabbing to a field out of the view.
    
    Added shitty selection menus for button fields. Right now they are just on the 3 job title fields.

---
+++ Assets/Scripts/resume/ResumeOptionButtons.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResumeOptionButtons : MonoBehaviour {
+	public UnityEngine.Canvas canvas;
+	public UnityEngine.UI.Button targetBTN;
+	public UnityEngine.UI.Button BTNoption1;
+	public UnityEngine.UI.Button BTNoption2;
+	public UnityEngine.UI.Button BTNoption3;
+	
+	// Use this for initialization
+	void Start () 
+	{
+			canvas=this.GetComponent<Canvas>();
+			
+			BTNoption1.onClick.AddListener(op1);
+			BTNoption2.onClick.AddListener(op2);
+			BTNoption3.onClick.AddListener(op3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void ActivateCanvas()
+	{
+		canvas.enabled =true;
+	}
+	public void DeactivateCanvas()
+	{
+		canvas.enabled =false;
+	}
+	
+	void op1()
+	{
+		targetBTN.GetComponentInChildren<Text>().text = BTNoption1.GetComponentInChildren<Text>().text;
+		DeactivateCanvas();
+	}
+	void op2()
+	{
+		targetBTN.GetComponentInChildren<Text>().text = BTNoption2.GetComponentInChildren<Text>().text;
+		DeactivateCanvas();
+	}
+	void op3()
+	{
+		targetBTN.GetComponentInChildren<Text>().text = BTNoption3.GetComponentInChildren<Text>().text;
+		DeactivateCanvas();
+	}
+}
--- Assets/Scripts/resume/ResumeTabNav.cs
+++ Assets/Scripts/resume/ResumeTabNav.cs
@@ -9,7 +9,7 @@ public class ResumeTabNav : MonoBehaviour
     EventSystem system;
 	public List<UnityEngine.UI.InputField> fields = new List<UnityEngine.UI.InputField>();
 	public int index = 500; //start index at something higher than list count so the first tab will jump to the first field
-	
+	public GameObject textObject;
     void Start()
     {
         system = EventSystem.current;// EventSystemManager.currentSystem;
@@ -30,5 +30,18 @@ public class ResumeTabNav : MonoBehaviour
 			
 			fields[index].ActivateInputField();
         }
+		
+		if (fields[0].isFocused == true)
+		{
+			textObject.transform.position= new Vector3(0,200,0);
+		}
+		if (fields[4].isFocused == true)
+		{
+			textObject.transform.position= new Vector3(0,850,0);
+		}
+		if (fields[7].isFocused == true)
+		{
+			textObject.transform.position= new Vector3(0,1400,0);
+		}
     }
 }
